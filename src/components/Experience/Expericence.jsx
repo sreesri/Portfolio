5fd0a2c99d3a84fef47a99d25e084be61a04ef48
Shriram <sri59776@gmail.com>
2022-08-06 14:08:08 +0530

    experience content

--- src/components/Experience/Expericence.jsx
+++ src/components/Experience/Expericence.jsx
@@ -12,14 +12,50 @@ function Expericence() {
           <div className="experience__Header">
             <h3>Maventic Innovative Solutions</h3>
             <div className="experience__content">
-              
-
+              <article className="experience__project">
+                <h4>Internship:</h4>
+                <small>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat.
+                </small>
+              </article>
+              <article className="experience__project">
+                <h4>Clarion360:</h4>
+                <small>
+                  &emsp;&emsp;Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                  sed do eiusmod tempor incididunt ut labore et dolore magna
+                  aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                  ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                </small>
+              </article>
             </div>
           </div>
         </div>
         <div className="experience__company">
           <div className="experience__Header">
             <h3>Infrrd.ai</h3>
+            <div className="experience__content">
+              <article className="experience__project">
+                <h4>Homegenius</h4>
+                <small>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat.
+                </small>
+              </article>
+              <article className="experience__project">
+                <h4>Zageno</h4>
+                <small>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat.
+                </small>
+              </article>
+            </div>
           </div>
         </div>
       </div>
