de99e3c43e13501f7677dc7868b584b0e4671b56
Shriram <sri59776@gmail.com>
2022-08-06 13:39:05 +0530

    experience

--- src/components/Experience/Expericence.jsx
+++ src/components/Experience/Expericence.jsx
@@ -2,7 +2,29 @@ import React from "react";
 import "./Experience.css";
 
 function Expericence() {
-  return <section id="Experience">Expericence</section>;
+  return (
+    <section id="Experience">
+      <h5>Want To Know ?</h5>
+      <h2>My Experience</h2>
+
+      <div className="container experience__container">
+        <div className="experience__company">
+          <div className="experience__Header">
+            <h3>Maventic Innovative Solutions</h3>
+            <div className="experience__content">
+              
+
+            </div>
+          </div>
+        </div>
+        <div className="experience__company">
+          <div className="experience__Header">
+            <h3>Infrrd.ai</h3>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Expericence;
