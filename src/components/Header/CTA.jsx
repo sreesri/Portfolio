2bc9a0bcd501c79d2175cdee27e68d8668c3fd85
Shriram <sri59776@gmail.com>
2022-08-18 17:25:33 +0530

    updated cv

--- src/components/Header/CTA.jsx
+++ src/components/Header/CTA.jsx
@@ -1,10 +1,14 @@
 import React from "react";
-import CV from "../../assets/cv.pdf";
+import CV from "../../assets/Shriram_MS_FullStack.pdf";
 function CTA() {
   return (
     <div className="cta">
-      <a className="btn" href={CV} download>Download CV</a>
-      <a className="btn btn-primary" href="#contact">Let's talk</a>
+      <a className="btn" href={CV} download>
+        Download CV
+      </a>
+      <a className="btn btn-primary" href="#contact">
+        Let's talk
+      </a>
     </div>
   );
 }
