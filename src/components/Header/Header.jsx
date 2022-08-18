5bac68b515e0358ddaf6e40e57ee3303e60eeb4f
Shriram <sri59776@gmail.com>
2022-08-18 17:35:34 +0530

    added links

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Header.css";
 import CTA from "./CTA";
-import ME from "../../assets/me.png"
+import ME from "../../assets/me.png";
 import HeaderSocials from "./HeaderSocials";
 
 function Header() {
@@ -11,13 +11,15 @@ function Header() {
         <h5>Hello I'm</h5>
         <h1>Shriram MS</h1>
         <hs className="text-light">Fullstack Developer</hs>
-        <CTA/>
-        <HeaderSocials/>
+        <CTA />
+        <HeaderSocials />
         <div className="me">
           <img src={ME} alt="" />
         </div>
 
-        <a href="#contact" className="scroll__down">Scroll Down</a>
+        <a href="#Contact" className="scroll__down">
+          Scroll Down
+        </a>
       </div>
     </header>
   );
--- src/components/Header/HeaderSocials.jsx
+++ src/components/Header/HeaderSocials.jsx
@@ -6,10 +6,10 @@ import { SiLeetcode } from "react-icons/si";
 function HeaderSocials() {
   return (
     <div className="header__socials">
-      <a href="linkedin" target="_blank">
+      <a href="https://www.linkedin.com/in/dev-shriram-ms/" target="_blank">
         <BsLinkedin />
       </a>
-      <a href="linkedin" target="_blank">
+      <a href="https://github.com/sreesri" target="_blank">
         <AiFillGithub />
       </a>
       <a href="linkedin" target="_blank">
